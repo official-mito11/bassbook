ddb7ccc338d76226bc550963be9598adb6fa194c
necpition205 <mswtester10417@gmail.com>
2025-11-15 20:21:18 +0900

    initial setup

---
+++ packages/core/src/utils/parser.ts
@@ -0,0 +1,7 @@
+export function parseToCSS(props: Record<string, any>): string {
+    return "";
+}
+
+export function parseToStyle(props: Record<string, any>): Record<string, any> {
+    return {};
+}
